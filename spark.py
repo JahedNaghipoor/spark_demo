a702a5f890b9b97ece53eceabc1d01649a15dde4
Jahed Naghipoor <jahednaghipoor1361@gmail.com>
2023-07-20 20:45:55 +0000

    pyspark works

--- spark.py
+++ spark.py
@@ -10,4 +10,4 @@ df = spark.createDataFrame([
     Row(a=2, b=3., c='string2', d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
     Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0))
 ])
-print(df)
+print(df.show())
